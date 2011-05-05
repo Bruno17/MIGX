741a48bc53d7aa8adedc34c0fc1ee18536d5d41b
Bruno Perner <b.perner@gmx.de>
2011-05-05 14:57:28 +0200

    it - lexicon
    Signed-off-by: Bruno Perner <b.perner@gmx.de>

---
+++ core/components/migx/lexicon/it/default.inc.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+
+ * migx
+
+ *
+
+ * @package migx
+
+ * @language it
+
+ */
+
+//$_lang['mig_'] = '';
+
+$_lang['mig_noitems'] = 'Nessun elemento trovato';
+
+$_lang['mig_add'] = 'Aggiungi Elemento';
+
+$_lang['mig_remove_confirm'] = 'Rimuovere Elemento?';
+
+$_lang['mig_edit'] = 'Modifica';
+
+$_lang['mig_remove'] = 'Rimuovi';
+
+$_lang['mig_duplicate'] = 'Duplica';
