b3b0216ca9c8d9df011ee0816f3cfd0deeb13d61
Bruno Perner <b.perner@gmx.de>
2011-05-27 10:34:06 +0200

    lexicon/it/tvprops.inc.php
    Signed-off-by: Bruno Perner <b.perner@gmx.de>

---
+++ core/components/migx/lexicon/it/tvprops.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * migx
+ *
+ * @package migx
+ * @language it
+ */
+
+
+$_lang['mig.tabs'] = 'Schede form';
+$_lang['mig.columns'] = 'Colonne griglia';
+$_lang['mig.btntext'] = 'Testo alternativo ad "Aggiungi Elemento"';
