0503f68879f4b6ec9697748a508cfcca6d8de90f
Bruno Perner <b.perner@gmx.de>
2012-07-31 08:17:20 +0200

    show warning-window before overriding xml-schemas
    Signed-off-by: Bruno Perner <b.perner@gmx.de>

--- core/components/migx/controllers/custom/packagemanager.php
+++ core/components/migx/controllers/custom/packagemanager.php
@@ -1,33 +1,66 @@
 <?php
 
 $customHandlers[] = "
-    updatePackage : function(task) {
-        var packageName = Ext.get('migxpm_packageName').dom.value;
-        var schema = '';
-        var prefix = '';
-        var usecustomprefix = 0;
-        if (task == 'saveSchema'){
-            schema = Ext.get('migxpm_schema').dom.value;
-        }
-        if (task == 'writeSchema' || task == 'createTables'){
-            usecustomprefix = Ext.getCmp('migxpm_use_custom_prefix').getValue();
-            prefix = Ext.get('migxpm_prefix').dom.value;
-        }        
-        MODx.Ajax.request({
-            url: Migx.config.connectorUrl
-            ,params: {
-                action: 'mgr/packagemanager/packagemanager'
-				,task: task
-                ,packageName : packageName
-                ,schema : schema
-                ,prefix : prefix
-                ,usecustomprefix : usecustomprefix
+updatePackage: function(task) {
+    var packageName = Ext.get('migxpm_packageName').dom.value;
+    var schema = '';
+    var prefix = '';
+    var usecustomprefix = 0;
+    var _this = this;
+    if (task == 'saveSchema') {
+        schema = Ext.get('migxpm_schema').dom.value;
+    }
+    if (task == 'writeSchema' || task == 'createTables') {
+        usecustomprefix = Ext.getCmp('migxpm_use_custom_prefix').getValue();
+        prefix = Ext.get('migxpm_prefix').dom.value;
+    }
+    if (task == 'writeSchema' || task == 'saveSchema') {
+        Ext.Msg.confirm(_('warning') || '', 'this will override the existing schema <br /> in your package:'+packageName+'<br /><br />Are you sure?', function(e) {
+            if (e != 'yes') {
+                return;
             }
-            ,listeners: {
-                'success': {fn:function(r){this.updatePackageSuccess(r)},scope:this}
+            MODx.Ajax.request({
+                url: Migx.config.connectorUrl,
+                params: {
+                    action: 'mgr/packagemanager/packagemanager',
+                    task: task,
+                    packageName: packageName,
+                    schema: schema,
+                    prefix: prefix,
+                    usecustomprefix: usecustomprefix
+                },
+                listeners: {
+                    'success': {
+                        fn: function(r) {
+                            _this.updatePackageSuccess(r)
+                        },
+                        scope: this
+                    }
+                }
+            });
+        });
+    } else {
+        MODx.Ajax.request({
+            url: Migx.config.connectorUrl,
+            params: {
+                action: 'mgr/packagemanager/packagemanager',
+                task: task,
+                packageName: packageName,
+                schema: schema,
+                prefix: prefix,
+                usecustomprefix: usecustomprefix
+            },
+            listeners: {
+                'success': {
+                    fn: function(r) {
+                        this.updatePackageSuccess(r)
+                    },
+                    scope: this
+                }
             }
         });
-    }	
+    }
+}
 ";
 
 $customHandlers[] = "
