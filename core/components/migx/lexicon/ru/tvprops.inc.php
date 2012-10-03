7f61b35ace2c78bd057b31665f2c9d8e11d6e753
Viktor Matushevskyi <viktorminator@gmail.com>
2012-10-04 01:53:46 +0300

    Lexicon added Russian lang

---
+++ core/components/migx/lexicon/ru/tvprops.inc.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * migx
+ *
+ * @package migx
+ * @language en
+ */
+
+
+$_lang['mig.tabs'] = 'Вкладки формы';
+$_lang['mig.columns'] = 'Разметка колонок';
+$_lang['mig.btntext'] = 'Замена кнопки "Добавить элемент"';
+$_lang['mig.previewurl'] = 'Просмотр Url';
+$_lang['mig.jsonvarkey'] = 'Просмотр JsonVarKey';
+$_lang['mig.configs'] = 'Конфигурации';
+$_lang['mig.autoResourceFolders'] = 'Auto Resource Folders';
