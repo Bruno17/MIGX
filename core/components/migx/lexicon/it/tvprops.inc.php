42c2e74388f233f23fcf16a176451b56cf80d9c1
Alessandro Tilli <web@tillilab.it>
2013-02-27 13:00:12 +0100

    Italian translation tvprops.inc

--- core/components/migx/lexicon/it/tvprops.inc.php
+++ core/components/migx/lexicon/it/tvprops.inc.php
@@ -7,9 +7,9 @@
  */
 
 
-$_lang['mig.tabs'] = 'Tabs Form';
+$_lang['mig.tabs'] = 'Schede form';
 $_lang['mig.columns'] = 'Colonne della Griglia';
-$_lang['mig.btntext'] = 'Sostituzione "Aggiungi Elemento"';
+$_lang['mig.btntext'] = 'Testo alternativo ad "Aggiungi Elemento"';
 $_lang['mig.previewurl'] = 'Url Anteprima';
 $_lang['mig.jsonvarkey'] = 'JsonVarKey Anteprima';
 $_lang['mig.configs'] = 'Configurazioni';
