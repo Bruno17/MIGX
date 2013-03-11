126d54818dac772c1cbc25e41881c91bef3e877e
Everett Griffiths <everett@fireproofsocks.com>
2013-03-11 10:39:54 -0600

    Added logging and error checking

--- core/components/migx/elements/snippets/migxResourceMediaPath.php
+++ core/components/migx/elements/snippets/migxResourceMediaPath.php
@@ -1,43 +1,85 @@
 <?php
-
+/**
+ * @name migxResourceMediaPath
+ * @description Dynamically calculates the upload path for a given resource
+ * 
+ * This Snippet is meant to dynamically calculate your baseBath attribute
+ * for custom Media Sources.  This is useful if you wish to shepard uploaded
+ * images to a folder dedicated to a given resource.  E.g. page 123 would 
+ * have its own images that page 456 could not reference.
+ *
+ * USAGE:
+ * [[migxResourceMediaPath? &pathTpl=`assets/businesses/{id}/`]]
+ *
+ * PARAMETERS
+ * &pathTpl string formatting string specifying the file path. 
+ *		Relative to MODX base_path
+ *		Available placeholders: {id}, {pagetitle}, {parent}
+ * &docid (optional) integer page id
+ * &createfolder (optional) boolean whether or not to create
+ */
 $pathTpl = $modx->getOption('pathTpl', $scriptProperties, '');
 $docid = $modx->getOption('docid', $scriptProperties, '');
 $createfolder = $modx->getOption('createFolder', $scriptProperties, false);
 $path = '';
 $createpath = false;
 
+if (empty($pathTpl)) {
+	$modx->log(MODX_LOG_LEVEL_ERROR, '[migxResourceMediaPath]: pathTpl not specified.');
+	return;
+}
+
 if (empty($docid) && $modx->getPlaceholder('docid')) {
     // placeholder was set by some script
-    $docid = $modx->getPlaceholder('docid');
+    // note, if you pass this a TV e.g. [[*parent]], it may not get parsed
+    $docid = $modx->getPlaceholder('docid'); 
 }
 if (empty($docid)) {
 
-    if (is_Object($modx->resource)) {
-        //on frontend
+	//on frontend
+    if (is_object($modx->resource)) {
         $docid = $modx->resource->get('id');
-    } else {
-        //on backend
+    }
+    //on backend 
+    else {
         $createpath = $createfolder;
+		// We do this to read the &id param from an Ajax request 
         $parsedUrl = parse_url($_SERVER['HTTP_REFERER']);
         parse_str($parsedUrl['query'], $parsedQuery);
 
         if (isset($parsedQuery['amp;id'])) {
-            $docid = $parsedQuery['amp;id'];
-        } elseif (isset($parsedQuery['id'])) {
-            $docid = $parsedQuery['id'];
-        }
+            $docid = (int) $parsedQuery['amp;id'];
+        } 
+        elseif (isset($parsedQuery['id'])) {
+            $docid = (int) $parsedQuery['id'];
+        }    
     }
 }
 
+if (empty($docid)) {
+	$modx->log(MODX_LOG_LEVEL_ERROR, '[migxResourceMediaPath]: docid could not be determined.');
+	return;
+}
+
 if ($resource = $modx->getObject('modResource', $docid)) {
-    $path = str_replace('{id}', $docid, $pathTpl);
-    $path = str_replace('{pagetitle}', $resource->get('pagetitle'), $pathTpl);
+	$path = $pathTpl;
+    $path = str_replace('{id}', $docid, $path);
+    $path = str_replace('{pagetitle}', $resource->get('pagetitle'), $path);
+    $path = str_replace('{parent}', $resource->get('parent'), $path);
     
     $fullpath = $modx->getOption('base_path') . $path;
 
     if ($createpath && !file_exists($fullpath)) {
-        mkdir($fullpath, 0755, true);
+        if(!@mkdir($fullpath, 0755, true)) {
+		    $modx->log(MODX_LOG_LEVEL_ERROR, sprintf('[migxResourceMediaPath]: could not create directory %s).', $fullpath));    
+        }
     }
 
     return $path;
 }
+else {
+	$modx->log(MODX_LOG_LEVEL_ERROR, sprintf('[migxResourceMediaPath]: resource not found (page id %s).', $docid));
+	return;
+}
+
+/*EOF*/
