740ff62f0dfbe9488917c70c9b205b2d0004feae
Jan Peca <theboxercz@gmail.com>
2013-04-09 15:04:18 +0200

    Czech localization

---
+++ core/components/migx/lexicon/cs/default.inc.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * migx
+ *
+ * @package migx
+ * @language cs
+ *
+ * @author modx.cz
+ */
+
+//$_lang['mig_'] = '';
+
+$_lang['migx'] = 'MIGX';
+$_lang['migx.management'] = 'MIGX Správa';
+$_lang['migx.management_desc'] = 'Spravujte Váš MIGX - konfigurace jsou zde. Můžete je upravovat kliknutím pravým tlačítkem na daný řádek.';
+
+$_lang['migx.noitems'] = 'Nebyly nalezeny žádné položky';
+$_lang['migx.add'] = 'Přidat položku';
+$_lang['migx.remove_confirm'] = 'Odstranit položku?';
+$_lang['migx.edit'] = 'Upravit';
+$_lang['migx.edit_raw'] = 'Edit raw';
+$_lang['migx.export_import'] = 'Export/Import';
+$_lang['migx.remove'] = 'Smazat';
+$_lang['migx.duplicate'] = 'Zkopírovat';
+$_lang['migx.preview'] = 'Náhled';
+$_lang['migx.save_resource'] = 'Před přidáním nové položky musíte nejprve uložit tento dokument!';
+$_lang['migx.save_object'] = 'Před přidáním nové položky musíte nejprve uložit tento objekt!';
+
+$_lang['migx.show_trash'] = 'Zobrazit odstraněné';
+$_lang['migx.show_normal'] = 'Normální pohled';
+$_lang['migx.loadgrid'] = 'Načíst tabulku';
+
+$_lang['migx.create'] = 'Vytvořit';
+$_lang['migx.publish'] = 'Publikovat';
+$_lang['migx.unpublish'] = 'Ukončit publikování';
+$_lang['migx.delete'] = 'Odstranit';
+$_lang['migx.actions'] = 'Akce';
+$_lang['migx.bulk_actions'] = 'Dávkové akce';
+$_lang['migx.publish_selected'] = 'Publikovat vybrané';
+$_lang['migx.unpublish_selected'] = 'Ukončit publikování vybraným';
+$_lang['migx.delete_selected'] = 'Odstranit vybrané';
+$_lang['migx.remove_selected'] = 'Vymazat vybrané';
+$_lang['migx.export_current_view'] = 'Exportovat aktuální pohled';
+$_lang['migx.recall'] = 'Obnovit';
+$_lang['migx.new'] = 'nový';
+$_lang['migx.filters'] = 'Filtry';
+
+$_lang['migx.add_replacement'] = '"Přidat položku" nahrazení';
+$_lang['migx.activate'] = 'Aktivovat';
+$_lang['migx.deactivate'] = 'Deaktivovat';
+
+$_lang['migx.import_from_package'] = 'Import z balíčku';
+$_lang['migx.export_to_package'] = 'Export do balíčku';
+
+$_lang['migx.month'] = 'Měsíc';
+$_lang['migx.year'] = 'Rok';
+
+$_lang['migx.upload_images'] = 'Nahrát soubory';
+$_lang['migx.load_from_source'] = 'Načíst ze zdroje médií';
+
+$_lang['migx.selector_options'] = 'Možnosti';
