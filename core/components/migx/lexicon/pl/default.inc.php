e8f165050c887c6c6c53f32b0810e76dc7ff14e5
Bruno17 <b.perner@gmx.de>
2014-01-17 09:19:07 +0100

    added polish translation

---
+++ core/components/migx/lexicon/pl/default.inc.php
@@ -0,0 +1,62 @@
+﻿<?php
+/**
+ * migx
+ *
+ * @package migx
+ * @language en
+ */
+
+//$_lang['mig_'] = '';
+
+$_lang['migx'] = 'Zmienne MIGX';
+$_lang['migx.management'] = 'Zarządzanie zmiennymi MIGX';
+$_lang['migx.management_desc'] = 'Zarządzanie konfiguracjami MIGX. Edycja p[o kliknięciu prawym przyciskiem myszy.';
+
+$_lang['migx.noitems'] = 'Nie znaleziono żadnych elementów ';
+$_lang['migx.add'] = 'Dodaj';
+$_lang['migx.remove_confirm'] = 'Usunać ?';
+$_lang['migx.edit'] = 'Edycja';
+$_lang['migx.edit_raw'] = 'Edycja czystego kodu';
+$_lang['migx.export_import'] = 'Export/Import';
+$_lang['migx.remove'] = 'Usuń';
+
+$_lang['migx.duplicate'] = 'Duplikuj';
+$_lang['migx.preview'] = 'Podgląd';
+$_lang['migx.save_resource'] = 'Przed dodaniem nowych elementów zachowaj bieżący dokument';
+$_lang['migx.save_object'] = 'Przed dodaniem nowych elementów zachowaj zminay w tym..';
+
+$_lang['migx.show_trash'] = 'Widok roboczy';
+$_lang['migx.show_normal'] = 'Widok normalny';
+$_lang['migx.loadgrid'] = 'Załaduj ';
+
+$_lang['migx.create'] = 'Utwórz';
+$_lang['migx.publish'] = 'Publikuj';//ver&ouml;ffentlichen
+$_lang['migx.unpublish'] = 'Zmień status na nieopublikowany';//'zur&uuml;ckziehen'
+$_lang['migx.delete'] = 'Usuń';
+$_lang['migx.actions'] = 'Akcje';
+$_lang['migx.bulk_actions'] = 'Bulk Actions';
+$_lang['migx.publish_selected'] = 'Publikuj zaznaczone';
+$_lang['migx.unpublish_selected'] = 'Zmień status zaznaczonych elementów na nieopublikowane';
+$_lang['migx.delete_selected'] = 'Usuń zaznaczone';
+$_lang['migx.remove_selected'] = 'Usuń zaznaczone'; 
+$_lang['migx.export_current_view'] = 'Eksportuj obecny stan';
+$_lang['migx.recall'] = 'Odwołaj';
+$_lang['migx.remove'] = 'Usuń';
+$_lang['migx.new'] = 'nowy';
+$_lang['migx.filters'] = 'Filtry';
+
+$_lang['migx.add_replacement'] = 'Tekst zamiast "Dodaj"';
+$_lang['migx.activate'] = 'Aktywuj';
+$_lang['migx.deactivate'] = 'Deaktywuj';
+
+$_lang['migx.import_from_package'] = 'Importuj z pakietu';
+$_lang['migx.export_to_package'] = 'Eksportuj do pakietu';
+
+$_lang['migx.month'] = 'Miesiąc';
+$_lang['migx.year'] = 'Rok';
+
+$_lang['migx.upload_images'] = 'Wgraj pliki';
+$_lang['migx.load_from_source'] = 'Załaduj z katalogu';
+$_lang['migx.max_records_alert'] = 'Maksymalna ilość rekordów';
+
+$_lang['migx.selector_options'] = 'Opcje';
---
+++ core/components/migx/lexicon/pl/tvprops.inc.php
@@ -0,0 +1,16 @@
+﻿<?php
+/**
+ * migx
+ *
+ * @package migx
+ * @language en
+ */
+
+
+$_lang['mig.tabs'] = 'Formularze danych';
+$_lang['mig.columns'] = 'Kolumny';
+$_lang['mig.btntext'] = 'Tekst zamiast "Dodaj" ';
+$_lang['mig.previewurl'] = 'Podgląd Url';
+$_lang['mig.jsonvarkey'] = 'Podgląd JsonVarKey';
+$_lang['mig.configs'] = 'Konfiguracje';
+$_lang['mig.autoResourceFolders'] = 'Opcja Auto Resource Folders';
