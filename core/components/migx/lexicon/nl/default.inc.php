60522dbb40956ba7bb167ed2209cdc9570d16e65
vierkantemeter <mark@vierkante-meter.nl>
2014-02-19 13:00:49 +0100

    Create default.inc.php

---
+++ core/components/migx/lexicon/nl/default.inc.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * migx
+ *
+ * @package migx
+ * @language nl
+ */
+
+//$_lang['mig_'] = '';
+
+$_lang['migx'] = 'MIGX';
+$_lang['migx.management'] = 'MIGX Management';
+$_lang['migx.management_desc'] = 'Beheer hier je MIGX configuraties. Je kunt ze bewerken door met de rechtermuisknop te klikken op de betreffende rij.';
+
+$_lang['migx.noitems'] = 'Geen items gevonden.';
+$_lang['migx.add'] = 'Voeg item toe';
+$_lang['migx.remove_confirm'] = 'Item verwijderen?';
+$_lang['migx.edit'] = 'Bewerk';
+$_lang['migx.edit_raw'] = 'Bewerk raw';
+$_lang['migx.export_import'] = 'Export/Import';
+$_lang['migx.remove'] = 'Verwijderen';
+$_lang['migx.duplicate'] = 'Dupliceren';
+$_lang['migx.preview'] = 'Voorbeeld';
+$_lang['migx.save_resource'] = 'Voordat je nieuwe items kunt toevoegen, moet je deze resource opslaan!';
+$_lang['migx.save_object'] = 'Voordat je nieuwe items kunt toevoegen, moet je dit object opslaan!';
+
+$_lang['migx.show_trash'] = 'Prullenbak weergave';
+$_lang['migx.show_normal'] = 'Normale weergave';
+$_lang['migx.loadgrid'] = 'Laad grid';
+
+$_lang['migx.create'] = 'Maken';
+$_lang['migx.publish'] = 'Publiceren';
+$_lang['migx.unpublish'] = 'Depubliceren';
+$_lang['migx.delete'] = 'Permanent verwijderen';
+$_lang['migx.actions'] = 'Acties';
+$_lang['migx.bulk_actions'] = 'Bulk acties';
+$_lang['migx.publish_selected'] = 'Publiceer geselecteerd';
+$_lang['migx.unpublish_selected'] = 'Depubliceer geselecteerd';
+$_lang['migx.delete_selected'] = 'Verwijder geselecteerd permanent';
+$_lang['migx.remove_selected'] = 'Verwijder geselecteerd'; 
+$_lang['migx.export_current_view'] = 'Exporteer huidige weergave';
+$_lang['migx.recall'] = 'Terugzetten';
+$_lang['migx.remove'] = 'Verwijderen';
+$_lang['migx.new'] = 'nieuw';
+$_lang['migx.filters'] = 'Filters';
+
+$_lang['migx.add_replacement'] = '"Voeg item toe" vervanging';
+$_lang['migx.activate'] = 'Activeren';
+$_lang['migx.deactivate'] = 'Deactiveren';
+
+$_lang['migx.import_from_package'] = 'Importeer uit package';
+$_lang['migx.export_to_package'] = 'Exporteer naar package';
+
+$_lang['migx.month'] = 'Maand';
+$_lang['migx.year'] = 'Jaar';
+
+$_lang['migx.upload_images'] = 'Upload bestanden';
+$_lang['migx.load_from_source'] = 'Laad uit mediasource';
+$_lang['migx.max_records_alert'] = 'Maximale aantal items bereikt';
+
+$_lang['migx.selector_options'] = 'Opties';
