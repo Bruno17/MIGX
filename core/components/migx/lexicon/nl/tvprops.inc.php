b4277f19db661e9e3e8c7c0f87c3c8869c48d836
vierkantemeter <mark@vierkante-meter.nl>
2014-02-19 13:03:25 +0100

    Create tvprops.inc.php

---
+++ core/components/migx/lexicon/nl/tvprops.inc.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * migx
+ *
+ * @package migx
+ * @language en
+ */
+
+
+$_lang['mig.tabs'] = 'Formulier tabs';
+$_lang['mig.columns'] = 'Grid kolommen';
+$_lang['mig.btntext'] = '"Voeg item toe" vervanging';
+$_lang['mig.previewurl'] = 'Voorbeeld URL';
+$_lang['mig.jsonvarkey'] = 'Voorbeeld JsonVarKey';
+$_lang['mig.configs'] = 'Configuraties';
+$_lang['mig.autoResourceFolders'] = 'Auto Resource Folders';
