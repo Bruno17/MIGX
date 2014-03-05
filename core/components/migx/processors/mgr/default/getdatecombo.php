c52fb2d8592a9d16e13214c8dc13a4318e78e30c
Bruno17 <b.perner@gmx.de>
2014-03-05 21:47:34 +0100

    extend date-filter

--- core/components/migx/processors/mgr/default/getdatecombo.php
+++ core/components/migx/processors/mgr/default/getdatecombo.php
@@ -5,8 +5,9 @@ $emtpytext = $config['gridfilters'][$scriptProperties['searchname']]['emptytext'
 $emtpytext = empty($emtpytext) ? 'all' : $emtpytext;
 $options = array();
 $options[] = array('combo_name' => $emtpytext, 'combo_id' => 'all');
-$options[] = array('combo_name' => '<', 'combo_id' => '<');
-$options[] = array('combo_name' => '>', 'combo_id' => '>');
+$options[] = array('combo_name' => '=', 'combo_id' => '=');
+$options[] = array('combo_name' => '<=', 'combo_id' => '<=');
+$options[] = array('combo_name' => '>=', 'combo_id' => '>=');
 
 $count = count($options);
 return $this->outputArray($options, $count);
