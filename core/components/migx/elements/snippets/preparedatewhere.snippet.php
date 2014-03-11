3f895008ce911ddfb34892982e060d3d4ff88175
Bruno17 <b.perner@gmx.de>
2014-03-11 15:53:26 +0100

    update snippet preparedatewhere

--- core/components/migx/elements/snippets/preparedatewhere.snippet.php
+++ core/components/migx/elements/snippets/preparedatewhere.snippet.php
@@ -1,7 +1,27 @@
-$name = $modx->getOption('name',$scriptProperties,'');
-$date = $modx->getOption($name.'_date',$_REQUEST,'');
-$dir = str_replace('T',' ',$modx->getOption($name.'_dir',$_REQUEST,'')) ;
+$name = $modx->getOption('name', $scriptProperties, '');
+$date = $modx->getOption($name . '_date', $_REQUEST, '');
+$dir = str_replace('T', ' ', $modx->getOption($name . '_dir', $_REQUEST, ''));
 
-if (!empty($date) && !empty($dir) && $dir != 'all'){
-    return '{"startdate:'.$dir.'":"'.$date.'"}';
+if (!empty($date) && !empty($dir) && $dir != 'all') {
+    switch ($dir) {
+        case '=':
+            $where = array(
+            'enddate:>=' => strftime('%Y-%m-%d 00:00:00',strtotime($date)),
+            'startdate:<=' => strftime('%Y-%m-%d 23:59:59',strtotime($date))
+            );
+            break;
+        case '>=':
+            $where = array(
+            'enddate:>=' => strftime('%Y-%m-%d 00:00:00',strtotime($date))
+            );
+            break;
+        case '<=':
+            $where = array(
+            'startdate:<=' => strftime('%Y-%m-%d 23:59:59',strtotime($date))
+            );            
+            break;
+
+    }
+
+    return $modx->toJson($where);
 }
