f26c14589088b3d2c16f1256b5b08adb43151da2
Bruno17 <b.perner@gmx.de>
2015-08-15 19:13:28 +0200

    [migxResourceMediapath] add more unique placeholder-name (mediasource_docid)

--- core/components/migx/elements/snippets/migxresourcemediapath.snippet.php
+++ core/components/migx/elements/snippets/migxresourcemediapath.snippet.php
@@ -31,6 +31,12 @@ if (empty($pathTpl)) {
     return;
 }
 
+if (empty($docid) && $modx->getPlaceholder('mediasource_docid')) {
+    // placeholder was set by some script
+    // warning: the parser may not render placeholders, e.g. &docid=`[[*parent]]` may fail
+    $docid = $modx->getPlaceholder('mediasource_docid');
+}
+
 if (empty($docid) && $modx->getPlaceholder('docid')) {
     // placeholder was set by some script
     // warning: the parser may not render placeholders, e.g. &docid=`[[*parent]]` may fail
@@ -86,32 +92,30 @@ if ($resource = $modx->getObject('modResource', $docid)) {
             $breadcrumbpath .= $parentids[$i] . '/';
         }
         $path = str_replace('{breadcrumb}', $breadcrumbpath, $path);
+    }
 
-    } else {
-        $path = str_replace('{id}', $docid, $path);
-        $path = str_replace('{pagetitle}', $resource->get('pagetitle'), $path);
-        $path = str_replace('{alias}', $resource->get('alias'), $path);
-        $path = str_replace('{parent}', $resource->get('parent'), $path);
-        $path = str_replace('{context_key}', $resource->get('context_key'), $path);
-        $path = str_replace('{ultimateparent}', $ultimateParent, $path);
-        if ($template = $resource->getOne('Template')) {
-            $path = str_replace('{templatename}', $template->get('templatename'), $path);
-        }
-        if ($user = $modx->user) {
-            $path = str_replace('{username}', $modx->user->get('username'), $path);
-        }
+    $path = str_replace('{id}', $docid, $path);
+    $path = str_replace('{pagetitle}', $resource->get('pagetitle'), $path);
+    $path = str_replace('{alias}', $resource->get('alias'), $path);
+    $path = str_replace('{parent}', $resource->get('parent'), $path);
+    $path = str_replace('{context_key}', $resource->get('context_key'), $path);
+    $path = str_replace('{ultimateparent}', $ultimateParent, $path);
+    if ($template = $resource->getOne('Template')) {
+        $path = str_replace('{templatename}', $template->get('templatename'), $path);
+    }
+    if ($user = $modx->user) {
+        $path = str_replace('{username}', $modx->user->get('username'), $path);
     }
 
     $fullpath = $modx->getOption('base_path') . $path;
-    
+
     if ($createpath && !file_exists($fullpath)) {
-        
+
         $permissions = octdec('0' . (int)($modx->getOption('new_folder_permissions', null, '755', true)));
         if (!@mkdir($fullpath, $permissions, true)) {
             $modx->log(MODX_LOG_LEVEL_ERROR, sprintf('[migxResourceMediaPath]: could not create directory %s).', $fullpath));
-        }
-        else{
-            chmod($fullpath, $permissions); 
+        } else {
+            chmod($fullpath, $permissions);
         }
     }
 
