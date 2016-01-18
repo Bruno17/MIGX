1794e749513d07da5f60e571ddc74fb646368faf
Steeve <lawebfabric@gmail.com>
2016-01-18 10:46:39 +0100

    Update default.inc.php
    Translation with double quote and double back slash

--- core/components/migx/lexicon/fr/default.inc.php
+++ core/components/migx/lexicon/fr/default.inc.php
@@ -3,40 +3,60 @@
  * migx
  *
  * @package migx
- * @language en
+ * @language fr
  */
-
 //$_lang['mig_'] = '';
-
-
-$_lang['mig_noitems'] = 'Aucun élément trouvé';
-$_lang['mig_add'] = 'Ajouter un élément';
-$_lang['mig_remove_confirm'] = 'Êtes-vous sûr de vouloir supprimer cet élément ?';
-$_lang['mig_edit'] = 'Éditer';
-$_lang['mig_remove'] = 'Supprimer';
-$_lang['mig_duplicate'] = 'Dupliquer';
-$_lang['mig_preview'] = 'Preview';
-$_lang['mig_save_resource'] = 'Before adding new items, you have to save this resource!';
-$_lang['mig_save_resource'] = 'Before adding new items, you have to save this resource!';
-
-$_lang['mig_show_trash'] = 'Trash View';
-$_lang['mig_show_normal'] = 'Normal View';
-
-$_lang['mig_loadgrid'] = 'load grid';
-
+$_lang['migx'] = 'MIGX';
+$_lang['migx.management'] = 'MIGX Management';
+$_lang['migx.management_desc'] = 'Gérez vos MIGX - Configurations ici. Vous pouvez les modifier en cliquant-droit sur la ligne respective.';
+$_lang['migx.noitems'] = 'Aucun élément trouvé';
+$_lang['migx.add'] = 'Ajouter un élément';
+$_lang['migx.remove_confirm'] = "Retirer l\\'élément ?";
+$_lang['migx.edit'] = 'Éditer';
+$_lang['migx.edit_raw'] = 'Editer la ligne';
+$_lang['migx.export_import'] = 'Exporter/Importer';
+$_lang['migx.remove'] = 'Retirer';
+$_lang['migx.duplicate'] = 'Dupliquer';
+$_lang['migx.preview'] = 'Prévisualiser';
+$_lang['migx.save_resource'] = "Avant d\\'ajouter de nouveaux éléments, vous devez enregistrer cette ressource !";
+$_lang['migx.save_object'] = "Avant d\\'ajouter de nouveaux éléments, vous devez enregistrer cet objet !";
+$_lang['migx.show_trash'] = 'Vue de la crobeille';
+$_lang['migx.show_normal'] = 'Vue normal';
+$_lang['migx.loadgrid'] = 'Chargement de la grille';
 $_lang['migx.create'] = 'Créer';
 $_lang['migx.publish'] = 'Publier';
 $_lang['migx.unpublish'] = 'Dépublier';
-$_lang['migx.delete'] = 'Delete';
+$_lang['migx.delete'] = 'Supprimer';
 $_lang['migx.actions'] = 'Actions';
 $_lang['migx.bulk_actions'] = 'Actions groupées';
-$_lang['migx.publish_selected'] = 'Publier la sélection';
-$_lang['migx.unpublish_selected'] = 'Dépublier la sélection';
-$_lang['migx.delete_selected'] = 'Supprimer la sélection';
-$_lang['migx.remove_selected'] = 'Enlever la sélection';
-$_lang['migx.export_current_view'] = 'Export Current View';
-$_lang['migx.recall'] = 'Recall';
-$_lang['migx.remove'] = 'Remove';
+$_lang['migx.publish_selected'] = 'Publier la selection';
+$_lang['migx.unpublish_selected'] = 'dépublier la selection';
+$_lang['migx.delete_selected'] = 'Supprimer la selection';
+$_lang['migx.remove_selected'] = 'Retirer la selection'; 
+$_lang['migx.export_current_view'] = 'Exporter la vue actuelle';
+$_lang['migx.recall'] = 'Rappeler';
+$_lang['migx.remove'] = 'Retirer';
 $_lang['migx.new'] = 'nouveau';
-$_lang['month'] = 'Mois';
-$_lang['year'] = 'Année';
+$_lang['migx.filters'] = 'Filtres';
+$_lang['migx.move_to_top'] = 'Déplacer en premier';
+$_lang['migx.move_to_bottom'] = 'Déplacer en dernier';
+$_lang['migx.add_replacement'] = 'Replacer "Ajouter un élément"';
+$_lang['migx.activate'] = 'Activer';
+$_lang['migx.deactivate'] = 'Désactiver';
+$_lang['migx.import_from_package'] = 'Importer depuis un package';
+$_lang['migx.export_to_package'] = 'Exporter vers un package';
+$_lang['migx.month'] = 'Mois';
+$_lang['migx.year'] = 'Année';
+$_lang['migx.upload_images'] = 'Télécharger des fichiers';
+$_lang['migx.load_from_source'] = 'Charger à partir du mediasource';
+$_lang['migx.max_records_alert'] = "Nombre maximal d\\'entrée atteintes";
+$_lang['migx.selector_options'] = 'Options';
+$_lang['migx.emptythrash_confirm'] = 'Vider la corbeille. Vous êtes sûr ?';
+$_lang['migx.emptythrash'] = 'Vider la corbeille';
+$_lang['migx.search'] = 'Recherche...';
+$_lang['migx.reset_all'] = 'Tout réinitialiser';
+$_lang['migx.import_csv'] = 'Importer un CSV';
+$_lang['migx.add_field'] = 'Ajouter un champ';
+$_lang['migx.add_formtab'] = 'Ajouter un onglet au formulaire';
+$_lang['migx.add_formlayout'] = 'Disposition';
+$_lang['migx.add_layoutcolumn'] = 'Disposition de la column';
