3c324492ea986e48ec178374dbda0d718f8ed009
Bruno17 <b.perner@gmx.de>
2019-06-15 11:03:55 +0200

    Finnish Translation

---
+++ core/components/migx/lexicon/fi/default.inc.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * migx
+ *
+ * @package migx
+ * @language fi
+ * Vesa Särkelä 2019
+ */
+//$_lang['mig_'] = '';
+$_lang['migx'] = 'MIGX';
+$_lang['migx.management'] = 'MIGX Hallinta';
+$_lang['migx.management_desc'] = 'Hallitse MIGX konfiguraatioita täällä. Voit muokata niitä napsauttamalla hiiren kakkospainikkeella vastaavaa riviä.';
+$_lang['migx.noitems'] = 'Kohteita ei löytynyt';
+$_lang['migx.add'] = 'Lisää kohde';
+$_lang['migx.remove_confirm'] = 'Poista kohde?'; 
+$_lang['migx.edit'] = 'Muokkaa';
+$_lang['migx.edit_raw'] = 'Muokkaa raakadataa';
+$_lang['migx.edit_flat'] = 'Flat formtabs';
+$_lang['migx.export_import'] = 'Vie/Tuo';
+$_lang['migx.remove'] = 'Poista';
+$_lang['migx.duplicate'] = 'Kopioi';
+$_lang['migx.preview'] = 'Esikatsele';
+$_lang['migx.save_resource'] = 'Ennen kuin lisäät uusia kohteita, sinun täytyy tallentaa tämä resurssi!';
+$_lang['migx.save_object'] = 'Ennen kuin lisäät uusia kohteita, sinun täytyy tallentaa tämä objekti!';
+$_lang['migx.show_trash'] = 'Roskakorinäkymä';
+$_lang['migx.show_normal'] = 'Normaali näkymä';
+$_lang['migx.loadgrid'] = 'Lataa ruudukko';
+$_lang['migx.create'] = 'Luo';
+$_lang['migx.publish'] = 'Julkaise';
+$_lang['migx.unpublish'] = 'Lopeta julkaiseminen';
+$_lang['migx.delete'] = 'Poista';
+$_lang['migx.actions'] = 'Toimet';
+$_lang['migx.bulk_actions'] = 'Joukkotoimet';
+$_lang['migx.publish_selected'] = 'Julkaise valitut';
+$_lang['migx.unpublish_selected'] = 'Lopeta valittujen julkaiseminen';
+$_lang['migx.delete_selected'] = 'Poista valitut';
+$_lang['migx.remove_selected'] = 'Poista valitut pysyvästi'; 
+$_lang['migx.export_current_view'] = 'Vie nykyinen näkymä';
+$_lang['migx.recall'] = 'Palauta';
+$_lang['migx.remove'] = 'Poista';
+$_lang['migx.new'] = 'uusi';
+$_lang['migx.filters'] = 'Suodattimet';
+$_lang['migx.move_to_top'] = 'Siirrä ylimmäiseksi';
+$_lang['migx.move_to_bottom'] = 'Siirrä alimmaiseksi';
+$_lang['migx.add_replacement'] = '"Lisää kohde" korvaaminen';
+$_lang['migx.activate'] = 'Aktivoi';
+$_lang['migx.deactivate'] = 'Poista käytöstä';
+$_lang['migx.import_from_package'] = 'Tuo paketista';
+$_lang['migx.export_to_package'] = 'Vie pakettiin';
+$_lang['migx.month'] = 'Kuukausi';
+$_lang['migx.year'] = 'Vuosi';
+$_lang['migx.upload_images'] = 'Lataa tiedostot';
+$_lang['migx.load_from_source'] = 'Lataa medialähteestä';
+$_lang['migx.max_records_alert'] = 'Maksimi määrä tietueita';
+$_lang['migx.selector_options'] = 'Vaihtoehdot';
+$_lang['migx.emptytrash_confirm'] = 'Tyhjennä roskat. Oletko varma?';
+$_lang['migx.emptytrash'] = 'Tyhjennä roskat';
+$_lang['migx.search'] = 'Hae...';
+$_lang['migx.reset_all'] = 'Nollaa kaikki';
+$_lang['migx.import_csv'] = 'Tuo CSV';
+$_lang['migx.add_field'] = 'Lisää kenttä';
+$_lang['migx.add_formtab'] = 'Lisää Lomake välilehti';
+$_lang['migx.add_formlayout'] = 'Asettelu';
+$_lang['migx.add_layoutcolumn'] = 'Asettelu-sarake';
+$_lang['migx.add_item_before'] = 'Lisää kohde ennen';
+$_lang['migx.add_item_after'] = 'Lisää kohde jälkeen';
+$_lang['migx.select_dbfields'] = 'Valitse db-kentät';
+$_lang['migx.select_dbfields_desc'] = 'Ennen kuin voit valita db-kentät, aseta jotain paketin Nimi-, luokka- ja liittymäasetuksiin ja tallenna konfiguraatio!';
---
+++ core/components/migx/lexicon/fi/tvprops.inc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * migx
+ *
+ * @package migx
+ * @language fi
+ * Vesa Särkelä 2019
+ */
+$_lang['mig.tabs'] = 'Lomake-välilehdet';
+$_lang['mig.columns'] = 'Ruudukon sarakkeet';
+$_lang['mig.btntext'] = '"Lisää kohde" korvaaminen';
+$_lang['mig.previewurl'] = 'Esikatsele URL-osoitetta';
+$_lang['mig.jsonvarkey'] = 'Esikatsele JsonVarKey';
+$_lang['mig.configs'] = 'Kokoonpanot';
+$_lang['mig.autoResourceFolders'] = 'Automaattiset resurssin kansiot';
