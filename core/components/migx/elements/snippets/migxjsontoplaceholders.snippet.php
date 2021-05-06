dee7a18446c27deffe22be77285cb87e773ce3cf
Hugo Peek <hugo@fractal-farming.com>
2021-05-06 14:32:06 +0800

    Process JSON array recursively

--- core/components/migx/elements/snippets/migxjsontoplaceholders.snippet.php
+++ core/components/migx/elements/snippets/migxjsontoplaceholders.snippet.php
@@ -4,9 +4,12 @@ $prefix = $modx->getOption('prefix',$scriptProperties,'');
 
 //$modx->setPlaceholders($modx->fromJson($value),$prefix,'',true);
 
-$values = $modx->fromJson($value);
+$values = json_decode($value, true);
+
+$it = new RecursiveIteratorIterator(new RecursiveArrayIterator($values));
+
 if (is_array($values)){
-    foreach ($values as $key => $value){
+    foreach ($it as $key => $value){
         $value = $value == null ? '' : $value;
         $modx->setPlaceholder($prefix . $key, $value);
     }
